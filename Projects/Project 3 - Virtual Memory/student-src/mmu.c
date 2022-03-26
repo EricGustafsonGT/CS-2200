4eba4f107d64a7af76474703389782d2d37ffdab
HeyItsMeEric <eric.a.gustafson@gmail.com>
2022-03-26 15:28:19 -0400

    Project 3 Initlialize

---
+++ Projects/Project 3 - Virtual Memory/student-src/mmu.c
@@ -0,0 +1,58 @@
+#include "mmu.h"
+#include "pagesim.h"
+#include "va_splitting.h"
+#include "swapops.h"
+#include "stats.h"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+/* The frame table pointer. You will set this up in system_init. */
+fte_t *frame_table;
+
+/**
+ * --------------------------------- PROBLEM 2 --------------------------------------
+ * Checkout PDF sections 4 for this problem
+ * 
+ * In this problem, you will initialize the frame_table pointer. The frame table will
+ * be located at physical address 0 in our simulated memory. You should zero out the 
+ * entries in the frame table, in case for any reason physical memory is not clean.
+ * 
+ * HINTS:
+ *      - mem: Simulated physical memory already allocated for you.
+ *      - PAGE_SIZE: The size of one page
+ * ----------------------------------------------------------------------------------
+ */
+void system_init(void) {
+    // TODO: initialize the frame_table pointer.
+}
+
+/**
+ * --------------------------------- PROBLEM 5 --------------------------------------
+ * Checkout PDF section 6 for this problem
+ * 
+ * Takes an input virtual address and performs a memory operation.
+ * 
+ * @param addr virtual address to be translated
+ * @param rw   'r' if the access is a read, 'w' if a write
+ * @param data If the access is a write, one byte of data to write to our memory.
+ *             Otherwise NULL for read accesses.
+ * 
+ * HINTS:
+ *      - Remember that not all the entry in the process's page table are mapped in. 
+ *      Check what in the pte_t struct signals that the entry is mapped in memory.
+ * ----------------------------------------------------------------------------------
+ */
+uint8_t mem_access(vaddr_t addr, char rw, uint8_t data) {
+    // TODO: translate virtual address to physical, then perfrom the specified operation
+
+    /* Either read or write the data to the physical address
+       depending on 'rw' */
+    if (rw == 'r') {
+
+    } else {
+
+    }
+
+    return 0;
+}
---
+++ Projects/Project 3 - Virtual Memory/student-src/page_fault.c
@@ -0,0 +1,39 @@
+#include "mmu.h"
+#include "pagesim.h"
+#include "swapops.h"
+#include "stats.h"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+/**
+ * --------------------------------- PROBLEM 6 --------------------------------------
+ * Checkout PDF section 7 for this problem
+ * 
+ * Page fault handler.
+ * 
+ * When the CPU encounters an invalid address mapping in a page table, it invokes the 
+ * OS via this handler. Your job is to put a mapping in place so that the translation 
+ * can succeed.
+ * 
+ * @param addr virtual address in the page that needs to be mapped into main memory.
+ * 
+ * HINTS:
+ *      - You will need to use the global variable current_process when
+ *      altering the frame table entry.
+ *      - Use swap_exists() and swap_read() to update the data in the 
+ *      frame as it is mapped in.
+ * ----------------------------------------------------------------------------------
+ */
+void page_fault(vaddr_t addr) {
+   // TODO: Get a new frame, then correctly update the page table and frame table
+
+
+   if(swap_exists(NULL)){
+
+   } else {
+
+   }
+}
+
+#pragma GCC diagnostic pop
